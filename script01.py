c6e045a7dced3205a41ad4749bb21fd28f752580
Matthew Hill <101017377+mattdhill011@users.noreply.github.com>
2022-04-09 13:44:32 -0400

    added some code to strict01

--- script01.py
+++ script01.py
@@ -1,1 +1,14 @@
 print("Hello, World")
+
+
+for n in range(1,100):
+    if n % 10 == 1:
+        cardinal = "st"
+    elif n % 10 == 2:
+        cardinal = "nd"
+    elif n % 10 == 3:
+        cardinal = "rd"
+    else:
+        cardinal = "th"
+    
+    print(f"This is the {n}{cardinal} test")
